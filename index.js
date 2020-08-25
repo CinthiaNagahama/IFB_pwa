24f257397c9d620d66383f79c5a409f276377928
Cinthia M. N. Ungefehr <ciin.nagahama@gmail.com>
2020-08-25 20:08:26 +0000

    Introdução a objetos (classe Pessoa))

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require("./modules/")
+const Pessoa = require("./modules/objects/Pessoa");
+
+const ana = new Pessoa(50, 1.55);
+console.log(ana.imc());
+
+const pedro = new Pessoa();
+pedro.peso = 80;
+pedro.altura = 1.78;
+console.log(pedro.imc());
